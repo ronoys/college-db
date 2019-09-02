44f787996952d4c6e5ef0e6cddc45c2c2c3291d9
ronoy <ronoysarkar.mail@gmail.com>
2019-09-02 15:37:25 -0400

    init firestore

---
+++ src/Firestore.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIuyHPL2TTXyX3xYQdQmj4Bzs8hl_Xtzg",
+    authDomain: "college-db-a39cb.firebaseapp.com",
+    databaseURL: "https://college-db-a39cb.firebaseio.com",
+    projectId: "college-db-a39cb",
+    storageBucket: "",
+    messagingSenderId: "669045417921",
+    appId: "1:669045417921:web:33c5616c7248e62a"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
