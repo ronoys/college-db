042a916481108ac74929fc93f0ef26a5467938f5
ronoy <ronoysarkar.mail@gmail.com>
2019-09-02 16:41:08 -0400

    slider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import './App.css';
 import College from './College.js';
+import Slide from './slider.js';
 
 function App() {
   return (
     <div className="App">
       
       <College></College>
-
+      <Slide></Slide>
 
 
     </div>
